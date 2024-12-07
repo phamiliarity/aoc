8f07cf5c04f87909a17b5752ccd8b6532cbb5796
Vina My Pham <146935351+phamiliarity@users.noreply.github.com>
2024-12-07 15:23:46 +0100

    upload day07

---
+++ 2024/day07_1.py
@@ -0,0 +1,27 @@
+def make_permutation_masks(length, current=""):
+    if length == 0:
+        return [current]
+    return (make_permutation_masks(length - 1, current + "+") +
+            make_permutation_masks(length - 1, current + "*"))
+
+data = [line.split(": ") for line in open('input.txt').read().splitlines()]
+data = [(int(ans), list(map(int, values.split()))) for ans, values in data]
+
+ans = 0
+for ans_check, values in data:
+    n_vals = len(values)
+    masks = make_permutation_masks(n_vals-1, current="")
+    
+    for mask in masks:
+        ans_calc = values[0]
+
+        for i in range(len(mask)):
+            if mask[i] == "*":
+                ans_calc = ans_calc * values[i+1]
+            if mask[i] == "+":
+                ans_calc = ans_calc + values[i+1]
+
+        if ans_calc == ans_check:
+            ans = ans + ans_check
+            break
+print(ans)
